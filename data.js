3671b4525ca2c306192c3c8e2a12b3ad86a3c06b
Daragh <daraghbland@gmail.com>
2020-02-11 23:29:21 +0000

    added absolute difference, other improvements

--- data.js
+++ data.js
@@ -2,6 +2,15 @@ export function Rotator() {
             
     this.rotate = function(imageData,radians){
 
+        if(imageData === null){
+            throw new Error("Please provide an imageData object");
+        }
+
+        // throw error if angle entered is out of range
+        if(radians < -6.28 || radians > 6.28) {
+            throw new Error("Please enter an acceptable radian value for rotation");
+        }
+
         // general iterators
         var i, j;
         
@@ -15,10 +24,10 @@ export function Rotator() {
 
         // create a source pixel array
         var srcImageData = new Uint32Array(imageData.data.buffer);
-        // create a destination pixel array
+        // create a destination pixel array (divide by 4 for rgba)
         var destImageData = new Uint32Array(imageData.data.length/4);
         
-        // Rotate the bitmap around the center
+        // Rotate the image around the center
         var iCentreX = iWidth / 2;
         var iCentreY = iHeight / 2;
         
@@ -40,7 +49,7 @@ export function Rotator() {
         for (i = 0; i < iDiagonal; ++i){
             for (j = 0; j < iDiagonal; ++j){
 
-                // convert raster to Cartesian
+                // convert matrix coordinates to Cartesian
                 x = j - iCentreX;
                 y = iCentreY - i;
 
@@ -48,41 +57,42 @@ export function Rotator() {
                 fDistance = Math.sqrt(x * x + y * y);
                 fPolarAngle = 0.0;
 
-            if (x === 0) {
-                if (y === 0) {
+                if (x === 0) {
+                    if (y === 0) {
 
-                    // centre of image, no rotation needed
-                    destImageData[j+i*iWidth] = srcImageData[j+i*iWidth];
-                    continue;
+                        // centre of image, no rotation needed
+                        destImageData[j+i*iWidth] = srcImageData[j+i*iWidth];
+                        continue;
                 
-                }else if (y < 0){
-                    fPolarAngle = 1.5 * Math.PI;
+                    }else if (y < 0){
+                        fPolarAngle = 1.5 * Math.PI;
+                    }else{
+                        fPolarAngle = 0.5 * Math.PI;
+                    }
                 }else{
-                    fPolarAngle = 0.5 * Math.PI;
+                    fPolarAngle = Math.atan2(parseFloat(y),parseFloat(x));
                 }
-            }else{
-                fPolarAngle = Math.atan2(parseFloat(y),parseFloat(x));
-            }
 
-            // Add the rotation
-            fPolarAngle += cnAngle;
+                // Add the rotation
+                fPolarAngle += cnAngle;
 
-            // convert polar to Cartesian
-            x = Math.floor((Math.round(fDistance * Math.cos(fPolarAngle))));
-            y = Math.floor((Math.round(fDistance * Math.sin(fPolarAngle))));
+                // convert polar to Cartesian
+                x = Math.floor((Math.round(fDistance * Math.cos(fPolarAngle))));
+                y = Math.floor((Math.round(fDistance * Math.sin(fPolarAngle))));
 
-            // convert Cartesian to raster
-            x = x + iCentreX;
-            y = iCentreY - y;
+                // convert Cartesian to matrix
+                x = x + iCentreX;
+                y = iCentreY - y;
 
-            // check bounds
-            if (x < 0 || x >= iWidth || y < 0 || y >= iHeight) continue;
+                // check bounds
+                if (x < 0 || x >= iWidth || y < 0 || y >= iHeight) continue;
 
-            destImageData[j + i * iWidth] = srcImageData[x + y * iWidth];
+                destImageData[j + i * iWidth] = srcImageData[x + y * iWidth];
             }
         }
 
-    srcImageData.set(destImageData);
-    return imageData;
+        // set the destination image coordinates against source
+        srcImageData.set(destImageData);
+        return imageData;
     }
 };
